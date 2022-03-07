4caebd6dc4b2ddde2d747cd2035afda41c6ebb6f
Niamh Boyle <2566489b@student.gla.ac.uk>
2022-03-07 16:08:03 +0000

    updated forms, models and settings. Forms and models now include correct params for fields

--- TwosComplement/forms.py
+++ TwosComplement/forms.py
@@ -1,25 +1,26 @@
 from django import forms
 from TwosComplement.models import User, Matches, Questionnaire
 
-Gender=['Male','Female','Prefer not to specify']
-Sexual_Preference=['Male','Female']
+Gender = ['Male', 'Female', 'Prefer not to specify']
+Sexual_Preference = ['Male', 'Female']
+
 
 class UserForm(forms.ModelForm):
-    username = forms.CharField(max_length=128, unique=True,required=True, help_text="Username already in use")
-    password = forms.PasswordInput(max_length=128, unique=True,required=True, help_text="Please enter password")
-    age = forms.IntegerField(max_length=2, required= True, help_text="Please enter your age number")
+    username = forms.CharField(max_length=128, required=True, help_text="Username already in use")
+    password = password = forms.CharField(widget=forms.PasswordInput())
+    age = forms.IntegerField(required=True, help_text="Please enter your age number")
     email = forms.EmailField(max_length=45, required=True, help_text="Please enter a valid email address")
     name = forms.CharField(max_length=128, required=True, help_text="Please enter your name")
-    phone = forms.IntegerField(default=11, help_text="Please enter a valid number and format")
-    photo = forms.ImageField(upload_to='profile_images', blank=True,)
+    phone = forms.IntegerField(help_text="Please enter a valid number and format")
+    #photo = forms.ImageField(upload_to='profile_images', blank=True,)
     bio = forms.CharField(max_length=254, help_text="Please enter a short description about yourself")
     gender = forms.CharField(label='Gender', widget=forms.Select(choices=Gender))
     sexualPreference = forms.CharField(label='Sexual preference', widget=forms.Select(choices=Sexual_Preference))
 
     class Meta:
         model = User
-        fields = ('username','password,','age','email','name','phone',
-        'photo','bio','gender','sexualPreference')
+        fields = ('username', 'password', 'age', 'email', 'name', 'phone',
+                  'photo', 'bio', 'gender', 'sexualPreference')
 
 
 
--- TwosComplement/models.py
+++ TwosComplement/models.py
@@ -3,13 +3,12 @@ from django.template.defaultfilters import slugify
 
 
 class User(models.Model):
-    NAME_MAX_LENGTH = 128
 
-    username = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
+    username = models.CharField(max_length=30, unique=True)
     password = models.CharField(max_length=30)
-    age = models.IntegerField(max_length=2)
+    age = models.IntegerField()
     email = models.EmailField(max_length=45)
-    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=False)
+    name = models.CharField(max_length=30)
     phone = models.IntegerField(default=11)
     photo = models.ImageField(upload_to='profile_images', blank=True)
     bio = models.CharField(max_length=254)
@@ -22,9 +21,9 @@ class User(models.Model):
 
 class Matches(models.Model):
 
-    user1 = models.ForeignKey(User, on_delete=models.CASCADE)
-    user2 = models.ForeignKey(User, on_delete=models.CASCADE)
-    compatibilityScore = models.IntegerField(max_length=2)
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user1")
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user2")
+    compatibilityScore = models.IntegerField()
 
     def __str__(self):
         return self.user1.username
@@ -32,14 +31,14 @@ class Matches(models.Model):
 
 class Questionnaire(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    answer1 = models.IntegerField(max_length=2)
-    answer2 = models.IntegerField(max_length=2)
-    answer3 = models.IntegerField(max_length=2)
-    answer4 = models.IntegerField(max_length=2)
-    answer5 = models.IntegerField(max_length=2)
-    answer6 = models.IntegerField(max_length=2)
-    answer7 = models.IntegerField(max_length=2)
-    answer8 = models.IntegerField(max_length=2)
+    answer1 = models.IntegerField()
+    answer2 = models.IntegerField()
+    answer3 = models.IntegerField()
+    answer4 = models.IntegerField()
+    answer5 = models.IntegerField()
+    answer6 = models.IntegerField()
+    answer7 = models.IntegerField()
+    answer8 = models.IntegerField()
 
     def __str__(self):
         return self.user.username
