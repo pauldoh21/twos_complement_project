b7146475410a20d67f5fded5d1c31d927c844a29
Ahmed Badawy <2613099b@student.gla.ac.uk>
2022-03-08 20:31:15 +0000

    questionaire form fixed

--- TwosComplement/forms.py
+++ TwosComplement/forms.py
@@ -6,16 +6,17 @@ from TwosComplement.models import UserProfile, Questionnaire
 Gender = (('0',''),('1','Male'),('2', 'Female'),('3', 'Prefer not to specify'))
 Sexual_Preference = (('0',''),('1','Male'),('2', 'Female'))
 
-Q1Choices = (('0',"Python"), ('1',"java"), ('2',"C and related"), ('3',"other"))
-Q2Choices = (('0',"VSCode"),('1',"Eclipse"),('2',"IntelliJ"),('3',"PyCharm"))
-Q3Choices = (('0',"< 1 year"),('1',"1-3 years"), ('2',"3-5 years"), ('3',"5+ years"))
-Q4Choices = (('0',"Apps"),('1',"Databases"),('2',"Games"),('3',"Security"))#look over options
-Q5Choices = (('0',"Artifical Intelligence"),('2',"Graphics"),('3',"Database Management Systems"),('4',"Human-Computer Interaction(HCI)"))
-Q6Choices = (('0',"Capricorn"),('1',"Aquarius"),('2',"Pisces"),('3',"Aries"),('4',"Taurus"),('5',"Gemimi"),('6',"Cancer"),('7',"Leo"),('8',"Virgo"),('9',"Libra"),('10',"Scorpio"),('11',"Sagittarius"))
-Q7Choices = (('0',"Extrovert"),('1',"Ambivert"),('2',"Introvert"))
-Q8Choices = (('0', "Physical Touch"),('1',"Word of Affirmation"),('2',"Quality Time"),('3',"Gift Giving"),('4',"Acts of Service"))
-Q9Choices = (('0',"Sports"),('1',"Travel"),('2',"Working"),('3',"Sleeping"),('4',"Clubbing"),('5',"TV/Movies"),('6',"Cooking"),('7',"Chilling"))
-Q10Choices = (('0',"Italian"),('1',"Indian"),('2',"Chinese"),('3',"Mexican"),('4',"other"),('5',"No Preference"))
+Q1Choices = (('0',''),('1',"Python"), ('2',"java"), ('3',"C and related"), ('4',"other"))
+Q2Choices = (('0',''),('1',"VSCode"),('2',"Eclipse"),('3',"IntelliJ"),('4',"PyCharm"))
+Q3Choices = (('0',''),('1',"< 1 year"),('2',"1-3 years"), ('3',"3-5 years"), ('4',"5+ years"))
+Q4Choices = (('0',''),('1',"Apps"),('2',"Databases"),('3',"Games"),('4',"Security"))#look over options
+Q5Choices = (('0',''),('1',"Artifical Intelligence"),('2',"Graphics"),('3',"Database Management Systems"),('4',"Human-Computer Interaction(HCI)"))
+Q6Choices = (('0',''),('1',"Capricorn"),('2',"Aquarius"),('3',"Pisces"),('4',"Aries"),('5',"Taurus"),('6',"Gemimi"),('7',"Cancer"),('8',"Leo"),('9',"Virgo"),('10',"Libra"),('11',"Scorpio"),('12',"Sagittarius"))
+Q7Choices = (('0',''),('1',"Extrovert"),('2',"Ambivert"),('3',"Introvert"))
+Q8Choices = (('0',''),('1', "Physical Touch"),('2',"Word of Affirmation"),('3',"Quality Time"),('4',"Gift Giving"),('5',"Acts of Service"))
+Q9Choices = (('0',''),('1',"Sports"),('2',"Travel"),('3',"Working"),('4',"Sleeping"),('5',"Clubbing"),('6',"TV/Movies"),('7',"Cooking"),('8',"Chilling"))
+Q10Choices = (('0',''),('1',"Italian"),('2',"Indian"),('3',"Chinese"),('4',"Mexican"),('5',"other"),('6',"No Preference"))
+
 
 class UserForm(forms.ModelForm):
     username = forms.CharField(max_length=30, required=True, help_text="Username" )
